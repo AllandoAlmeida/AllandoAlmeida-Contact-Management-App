a32f64c867078fba69ed3439b63415f5c7b484d4
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 13:38:31 -0300

    feat: prodution

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3009/api";
-export const API_BASE_URL_TEST = "https://blog-fake-api.onrender.com";
+export const API_BASE_URL = "https://contact-management-api-lac0.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
