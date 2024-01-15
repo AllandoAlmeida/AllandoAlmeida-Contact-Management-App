75a7a4b5e6f399fc489605195d612683c15036ff
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 13:54:16 -0300

    feat: base_url

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://contact-management-api-lac0.onrender.com";
+export const API_BASE_URL = "https://contact-management-api-lac0.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
