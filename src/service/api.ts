fa5af429ff1c216316e9e25d28a2c4e68562d55c
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 14:18:52 -0300

    fix: fix bug api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+/* export const API_BASE_URL = "https://localhost:3009/api"; */
 export const API_BASE_URL = "https://contact-management-api-lac0.onrender.com/api";
+export const API_BASE_URL_TEST = "https://blog-fake-api.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
