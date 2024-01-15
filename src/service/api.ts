464c27b48ce33c3a0311afe8d286afb781e61d98
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-15 16:05:08 -0300

    feat: url deploy

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-/* export const API_BASE_URL = "https://localhost:3009/api"; */
-export const API_BASE_URL = "https://contact-management-api-lac0.onrender.com/api";
-export const API_BASE_URL_TEST = "https://blog-fake-api.onrender.com";
+/* export const API_BASE_URL = "http://localhost:3009/api"; */
+
+export const API_BASE_URL= "https://contact-management-api-lac0.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 8000,
+  timeout: 20000,
 });
